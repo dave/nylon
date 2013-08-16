95642c98ac7db595b6b6caa265d4b626a8e183be
David Brophy <davebrophy@spotify.com>
2013-08-16 15:42:30 +0200

    full render method
    Signed-off-by: David Brophy <davebrophy@spotify.com>

--- src/foo/foo.go
+++
@@ -1,5 +0,0 @@
-package foo
-
-func Get() string {
-	return "foo"
-}
---
+++ src/store/store.go
@@ -0,0 +1,11 @@
+package store
+
+func Get() string {
+	return `
+<html>
+	<body>
+		<h1>Hello world</h1>
+	</body>
+</html>
+`
+}
